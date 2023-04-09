184dcfb9dac46bcdbdd2ca9c93efb6f6d7d02fd9
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2023-04-09 12:23:56 -0600

    Clase terminada

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+import {Cliente} from './Cliente.js'
+import {CuentaCorriente} from './CuentaCorriente.js';
 
+const cliente = new Cliente('Leonardo', '123445', '2121212');
+const cliente2 = new Cliente('Maria', '20202', '09876');
+
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
+
+console.log(cliente);
+console.log(cliente);
+console.log(cuentaDeLeonardo);
+console.log(cuentaDeMaria);
+
+/* let saldo = cuentaDeLeonardo.verSaldo();
+
+saldo = cuentaDeLeonardo.depositoEnCuenta(150);
+console.log("El saldo actual de leonardo " + saldo);
+ */
+
+
+
+/* cuentaDeLeonardo.transferirParaCuenta(100, cuentaDeMaria);
+
+const saldoMaria = cuentaDeMaria.verSaldo();
+console.log("El saldo actual de Maria " + saldoMaria);
+
+const saldoLeonardo = cuentaDeLeonardo.verSaldo();
+console.log("El saldo actual de leonardo " + saldoLeonardo); */
